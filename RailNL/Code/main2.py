6f7ef9f9e52968d1e55037cb13d171d1700f1004
[MattiaSabino] <[Mattiasabinocaso@gmail.com]>
2017-12-18 17:47:48 +0100

    'h'

--- RailNL/Code/main2.py
+++ RailNL/Code/main2.py
@@ -5,8 +5,7 @@
 #Dit is het main bestand. Run dit bestand door middel van main.py.
 #
 
-import functies.scorefunctie
-import algo.trajectmaker
+
 import functies.minuten
 import algo.hillclimber
 import inladen.inladen
@@ -32,7 +31,7 @@ MAX = 180
 
 MAX2 = 180
 
-
+SCORE = 0
 
 # Aantal trajecten.
 RANGE = 10
@@ -60,34 +59,8 @@ graph = inladen.inladen.graph(stations, alle_sporen)
 # Pak alle uithoeken. 
 uithoeken = inladen.inladen.uithoeken(graph, stations)
 
-# Maak de eerste oplossing en indelen. 
-trajecten = algo.trajectmaker.traject_maker(RANGE, MAX, stations, verbindingen, uithoeken, graph, TOTAAL_SPOREN, TOTAAL_STATIONS)
-
-alle_tijdsduur_oud = trajecten[0]
-alle_trajecten_oud = trajecten[1]
-sporen_oud = trajecten[2]
-trajecten_algemeen_oud = trajecten[3]
-
-
-trajecten = functies.opschonen.opschonen(alle_trajecten_oud, alle_tijdsduur_oud,  verbindingen)
-
-alle_trajecten_oud = trajecten[0] 
-alle_tijdsduur_oud = trajecten[1] 
-
-# Bereken de score.
-totale_tijdsduur_oud = functies.minuten.minuten(alle_tijdsduur_oud)
-score_oud = functies.scorefunctie.score(alle_trajecten_oud, totale_tijdsduur_oud, sporen_oud, TOTAAL_SPOREN)
-
-print()
-print("Nearest Neigbor")
-print("SCORE WAS: ", score_oud)
-print("AANTAL SPOREN WAS: ", len(sporen_oud))
-print("AANTAL STATIONS WAS: ", len(trajecten_algemeen_oud))
-print("TOTAAL AANTAL MINUTEN WAS: ", totale_tijdsduur_oud)
-print()
-
 # Pas de hillclimber toe.
-resultaat = algo.hillclimber.hillclimber(score_oud, alle_trajecten_oud, alle_tijdsduur_oud, HILL, RANGE, MAX, stations, verbindingen, uithoeken, graph, trajecten_algemeen_oud, sporen_oud, TOTAAL_SPOREN, TOTAAL_STATIONS)
+resultaat = algo.hillclimber.hillclimber(SCORE, HILL, RANGE, MAX, stations, verbindingen, uithoeken, graph, TOTAAL_SPOREN, TOTAAL_STATIONS)
 
 # Hill climber returnd 4 gegevens. Deze worden weer opgehaald. 
 score = resultaat[0]
@@ -117,6 +90,22 @@ print(time.clock() - start_time, "seconden")
 
 
 #visualisatie.visualisatie.visualisatie(alle_trajecten, STATIONS, VERBINDINGEN)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
